2bda1669b416d24b947501a85995696eb0d549d7
Chris Poulter <cpoulter@hotmail.co.uk>
2024-10-27 11:27:23 +0000

    add user content components

---
+++ src/app/user/create/content.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+import { Container } from '@/components/container';
+import { Title, SubTitle } from '@/components/title';
+import { ButtonLink } from '@/components/button-link';
+import {
+    CreateUserForm,
+    CreateUserFormValues
+} from '@/features/user/components/create-user-form';
+import { useCreateUser } from '@/features/user/hooks/use-create-user';
+
+export default function CreateUserContent() {
+    const router = useRouter();
+
+    const { mutate, isPending } = useCreateUser();
+
+    const onSubmit = (values: CreateUserFormValues) =>
+        mutate(values, {
+            onSuccess: async () => {
+                toast.success('User successfully created.');
+                return router.push('/user');
+            }
+        });
+
+    return (
+        <Container>
+            <Title>
+                User
+                <SubTitle>Create</SubTitle>
+            </Title>
+
+            <CreateUserForm
+                isLoading={isPending}
+                onSubmit={onSubmit}
+                options={
+                    <ButtonLink href="/user" variant="secondary">
+                        Cancel
+                    </ButtonLink>
+                }
+            />
+        </Container>
+    );
+}
