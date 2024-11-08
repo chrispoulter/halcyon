1659843e604d163960cae80a65e951db64271397
Chris Poulter <cpoulter@hotmail.co.uk>
2024-11-08 12:46:31 +0000

    update mocks

--- vitestSetup.ts
+++ vitestSetup.ts
@@ -2,6 +2,12 @@ import { vi } from 'vitest';
 
 vi.mock('next/router', () => vi.importActual('next-router-mock'));
 
+vi.mock('next-auth', () => ({
+    default: () => ({
+        auth: vi.fn()
+    })
+}));
+
 vi.mock('next-auth/react', () => ({
     useSession: vi.fn(() => ({})),
     signIn: vi.fn(),
