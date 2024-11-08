9200ffb5256f49a80bbd2e5a8db59d05715d42f9
Chris Poulter <cpoulter@hotmail.co.uk>
2024-11-08 12:42:27 +0000

    mock lib auth

--- vitestSetup.ts
+++ vitestSetup.ts
@@ -4,5 +4,10 @@ vi.mock('next/router', () => vi.importActual('next-router-mock'));
 
 vi.mock('next-auth/react', () => ({
     useSession: vi.fn(() => ({})),
-    signIn: vi.fn()
+    signIn: vi.fn(),
+    signOut: vi.fn()
+}));
+
+vi.mock('@/lib/auth', () => ({
+    auth: vi.fn()
 }));
